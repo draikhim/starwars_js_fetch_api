f05e76b401b89149531460975a9afeff5f883e94
Dhan Rai <draikhim@gmail.com>
2023-02-12 23:08:27 -0500

    add header

--- app.js
+++ app.js
@@ -17,15 +17,23 @@ async function getUsers() {
 async function renderUsers() {
     let users = await getUsers();
     let html = '';
+    let htmlHeader = `<div style="border-bottom: solid 2px skyblue;">
+                        <span style="padding-right: 20px; border-right: solid 2px skyblue;">Index</span>
+                        <span style="padding-right: 20px; border-right: solid 2px skyblue;">Name</span>
+                        <span style="padding-right: 20px; border-right: solid 2px skyblue;">Height</span>
+                        <span style="padding-right: 20px; border-right: solid 2px skyblue;">Gender</span>
+                        <span style="padding-right: 20px; border-right: solid 2px skyblue;">Film URLs</span>
+                    </div>`;
     users.forEach((user, index) => {
         // let username = ${user.name};
         // let url2 = 'https://swapi.dev/api/people/'
         // console.log("URL 2", url2);
         let htmlSegment = `<div class="user" style="border-bottom: solid 2px skyblue;">
-                <span style="padding-right: 20px; border-right: solid 2px skyblue;">${index}</span>
-                <span style="padding-right: 20px; border-right: solid 2px skyblue;">${user.name}</span>
-                <span style="padding-right: 20px; border-right: solid 2px skyblue;">${user.height}</span>
-                <span style="padding-right: 20px; border-right: solid 2px skyblue;">${user.mass}</span>`;
+                                <span style="padding-right: 48px; border-right: solid 2px skyblue;">${index}</span>
+                                <span style="padding-right: 20px; border-right: solid 2px skyblue;">${user.name}</span>
+                                <span style="padding-right: 20px; border-right: solid 2px skyblue;">${user.height}</span>
+                                <span style="padding-right: 20px; border-right: solid 2px skyblue;">${user.gender}</span>
+                            </div>`;
 
         user.films.forEach(film => {
             let film_list = [];
@@ -38,7 +46,7 @@ async function renderUsers() {
     });
 
     let container = document.querySelector('.container');
-    container.innerHTML = html;
+    container.innerHTML = htmlHeader + html;
 }
 
 renderUsers();
